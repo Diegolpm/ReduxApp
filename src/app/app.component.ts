04714bc8dd0325ffb5102a2a9b5b240359ea6958
Diego Walter Pajuelo Menacho <diego.pajuelo@avantica.net>
2021-06-09 17:39:26 -0500

    feat: store reducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Decrementar, Incrementar } from './contador/ngrx/contador.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux-app';
+  contador: number;
+  contador$: Observable<number>
+
+  constructor(private store: Store<{ contador: number }>) {
+    this.contador$ = store.select('contador');
+  }
+
+  incrementar() {
+    this.store.dispatch(Incrementar());
+    // this.contador += 1;
+    // this.contador ++;
+  }
+
+  decrementar() {
+    this.store.dispatch(Decrementar());
+    // this.contador -= 1;
+    // this.contador --;
+  }
+
+
 }
---
+++ src/app/contador/nieto/nieto.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Reset } from '../ngrx/contador.actions';
+
+
+@Component({
+  selector: 'app-nieto',
+  templateUrl: './nieto.component.html',
+  styleUrls: ['./nieto.component.scss']
+})
+export class NietoComponent implements OnInit {
+  @Input() contador: number;
+  @Output() contadorCambio = new EventEmitter<number>();
+  contador$: Observable<number>;
+
+  constructor(private store: Store<{ contador: number }>) { }
+
+  ngOnInit(): void {
+    this.contador$ = this.store.select('contador');
+  }
+
+  reset() {
+    this.store.dispatch(Reset());
+    // this.contador = 0;
+    // this.contadorCambio.emit(0);
+  }
+
+}
